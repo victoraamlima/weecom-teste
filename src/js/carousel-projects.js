53b4d00bb239b04e0540bb48c47b15b4addac8b7
Victor Lima <victoraamlima@gmail.com>
2023-08-25 14:38:08 -0300

    bug fix

--- src/js/carousel-projects.js
+++ src/js/carousel-projects.js
@@ -3,29 +3,22 @@ const carouselProjects = document.getElementsByClassName("carousel-projects")[0]
 const leftButtonProjects = document.getElementsByClassName(
   "left-button-projects"
 )[0]
-const leftArrowProjects = document.getElementById(
-  "left-arrow-projects"
-)
+const leftArrowProjects = document.getElementById("left-arrow-projects")
 
 const rightButtonProjects = document.getElementsByClassName(
   "right-button-projects"
 )[0]
-const rightArrowProjects = document.getElementById(
-  "right-arrow-projects"
-)
-
-let windowWidth
-let carouselWidth
+const rightArrowProjects = document.getElementById("right-arrow-projects")
 
 rightButtonProjects.addEventListener("click", () => {
-  windowWidth = window.innerWidth
-  carouselWidth = carouselProjects.offsetWidth
+  let windowWidth = window.innerWidth
+  let carouselWidth = carouselProjects.offsetWidth
 
   let marginLeft = carouselWidth - 1920 + "px"
 
   let disableRightButton = rightButtonProjects.classList.length === 3
 
-  if (carouselWidth <= 1920 && !disableRightButton) {
+  if (carouselWidth < 1920 && !disableRightButton) {
     carouselProjects.style.marginLeft = marginLeft
 
     rightButtonProjects.classList.add("disable")
@@ -44,7 +37,7 @@ leftButtonProjects.addEventListener("click", () => {
 
   let disableLeftButton = leftButtonProjects.classList.length === 3
 
-  if (carouselWidth <= 1920 && !disableLeftButton) {
+  if (carouselWidth < 1920 && !disableLeftButton) {
     carouselProjects.style.marginLeft = marginLeft
 
     leftButtonProjects.classList.add("disable")
