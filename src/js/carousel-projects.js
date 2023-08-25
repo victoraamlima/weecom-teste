822843d79f4422a9bf0270e03d4d8978e129ca5f
Victor Lima <victoraamlima@gmail.com>
2023-08-25 14:16:59 -0300

    add carousel-projects roll

---
+++ src/js/carousel-projects.js
@@ -0,0 +1,56 @@
+const carouselProjects = document.getElementsByClassName("carousel-projects")[0]
+
+const leftButtonProjects = document.getElementsByClassName(
+  "left-button-projects"
+)[0]
+const leftArrowProjects = document.getElementsByClassName(
+  "arrow-carousel left-arrow"
+)[0]
+
+const rightButtonProjects = document.getElementsByClassName(
+  "right-button-projects"
+)[0]
+const rightArrowProjects = document.getElementsByClassName(
+  "arrow-carousel right-arrow"
+)[0]
+
+let windowWidth
+let carouselWidth
+
+rightButtonProjects.addEventListener("click", () => {
+  windowWidth = window.innerWidth
+  carouselWidth = carouselProjects.offsetWidth
+
+  let marginLeft = carouselWidth - 1920 + "px"
+
+  let disableRightButton = rightButtonProjects.classList.length === 3
+
+  if (carouselWidth <= 1920 && !disableRightButton) {
+    carouselProjects.style.marginLeft = marginLeft
+
+    rightButtonProjects.classList.add("disable")
+    rightArrowProjects.classList.add("disable")
+
+    leftButtonProjects.classList.remove("disable")
+    leftArrowProjects.classList.remove("disable")
+  }
+})
+
+leftButtonProjects.addEventListener("click", () => {
+  windowWidth = window.innerWidth
+  carouselWidth = carouselProjects.offsetWidth
+
+  let marginLeft = 0
+
+  let disableLeftButton = leftButtonProjects.classList.length === 3
+
+  if (carouselWidth <= 1920 && !disableLeftButton) {
+    carouselProjects.style.marginLeft = marginLeft
+
+    leftButtonProjects.classList.add("disable")
+    leftArrowProjects.classList.add("disable")
+
+    rightButtonProjects.classList.remove("disable")
+    rightArrowProjects.classList.remove("disable")
+  }
+})
